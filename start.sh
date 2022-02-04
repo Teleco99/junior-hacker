56ac0993736098b8c940fba7c991dcfa8c66be7f
Dominik Pilár <445537@mail.muni.cz>
2022-02-04 15:06:51 +0100

    Added specific arguments to start script needed for integration with KYPO CRP.

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 #
 # Start the sandbox with student ID
 
-if [ ! $# -eq 1 ]; then
-    echo "Usage: ./start.sh student_ID"
+if [ ! $# -eq 3 ]; then
+    echo "Usage: ./start.sh student_id access_token syslog_server_IP"
     exit 1
 else
-    ANSIBLE_ARGS="--extra-vars \"student_id=$1\"" vagrant up
+    ANSIBLE_ARGS="--extra-vars \"student_id=$1\" --extra-vars \"access_token=$2\" --extra-vars \"central_syslog_ip=$3\"" vagrant up
 fi
